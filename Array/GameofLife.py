32666719d915abf849dd267c5cf8b075d19cd77f
ChandniBhadarka <chandnibhadarka@gmail.com>
2025-08-06 23:37:25 +0530

    Game of life

---
+++ Array/GameofLife.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def gameOfLife(self, board):
+        m, n = len(board), len(board[0])
+        
+        # Directions for 8 neighbors
+        directions = [(-1, -1), (-1, 0), (-1, 1),
+                    (0, -1),         (0, 1),
+                    (1, -1),  (1, 0), (1, 1)]
+        
+        for i in range(m):
+            for j in range(n):
+                live_neighbors = 0
+                
+                for dx, dy in directions:
+                    ni, nj = i + dx, j + dy
+                    if 0 <= ni < m and 0 <= nj < n and abs(board[ni][nj]) == 1:
+                        live_neighbors += 1
+                
+                # Rule 1 or 3: live cell dies
+                if board[i][j] == 1 and (live_neighbors < 2 or live_neighbors > 3):
+                    board[i][j] = -1  # Mark as was live, now dead
+                
+                # Rule 4: dead cell becomes live
+                if board[i][j] == 0 and live_neighbors == 3:
+                    board[i][j] = 2  # Mark as was dead, now live
+        
+        # Finalize board by converting -1 to 0 and 2 to 1
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] > 0:
+                    board[i][j] = 1
+                else:
+                    board[i][j] = 0
