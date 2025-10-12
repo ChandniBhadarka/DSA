8b6665510ed67ddc88ae4c338b0d4fd3900cb5ba
Chandni Bhadarka <92981317+ChandniBhadarka@users.noreply.github.com>
2025-10-12 10:33:44 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (99.00%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,8 +1,19 @@
 class Solution(object):
     def missingNumber(self, nums):
-        nums.sort()
-        for i in range(len(nums)):
-            if nums[i] != i:
-                return i
-        return len(nums)
+        #method 1 -> sorting
+        # nums.sort()
+        # for i in range(len(nums)):
+        #     if nums[i] != i:
+        #         return i
+        # return len(nums)
+
+        #method 2 -> XOR
+        n = len(nums)
+        res = 0
+        for i in range(n + 1):  
+            res ^= i
+        for num in nums:
+            res ^= num
+        return res
+
         
