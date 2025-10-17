c31e544bd38e5cd7ed79d55ede93aa719ebbbbba
Chandni Bhadarka <92981317+ChandniBhadarka@users.noreply.github.com>
2025-10-17 20:38:31 +0530

    Time: 45 ms (38.01%) | Memory: 26.9 MB (72.63%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,24 +1,24 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums):
-        # n = len(nums)
-        # hashmap = {}
+        n = len(nums)
+        hashmap = {}
         
-        # for num in nums:
-        #     hashmap[num] = True 
+        for num in nums:
+            hashmap[num] = True 
         
-        # missing = []
-        # for i in range(1, n + 1):
-        #     if i not in hashmap:
-        #         missing.append(i)
+        missing = []
+        for i in range(1, n + 1):
+            if i not in hashmap:
+                missing.append(i)
         
-        # return missing
+        return missing
 
-        nums.sort()
-        n=len(nums)
-        nums=set(nums)
-        res=[]
-        for i in range(1,n+1):
-            if i not in nums:
-                res.append(i)
-        return res
+        # nums.sort()
+        # n=len(nums)
+        # nums=set(nums)
+        # res=[]
+        # for i in range(1,n+1):
+        #     if i not in nums:
+        #         res.append(i)
+        # return res
         
