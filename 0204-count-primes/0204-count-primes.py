edb93e7a24d7237b42156808ed5e4eaf41044eb4
Chandni Bhadarka <92981317+ChandniBhadarka@users.noreply.github.com>
2026-03-15 20:27:18 +0530

    Time: 2423 ms (84.7%), Space: 130.1 MB (88.94%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -3,13 +3,13 @@ class Solution(object):
 
         if n <= 2:
             return 0
-
-        prime = [True] * n
-        prime[0] = prime[1] = False
-
-        for i in range(2, int(n**0.5) + 1):
-            if prime[i]:
-                for j in range(i*i, n, i):
-                    prime[j] = False
-
-        return sum(prime)
+        isprime= [True]*n
+        isprime[0]= isprime[1]= False
+        
+        p=2
+        while p*p < n:
+            if isprime[p]:
+                for i in range(p*p, n, p):
+                    isprime[i] = False
+            p +=1
+        return sum(isprime) 
