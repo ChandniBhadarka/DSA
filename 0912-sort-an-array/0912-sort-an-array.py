b46e4ebcccbf1686c1694e7f6d2ae87dc21659b9
Chandni Bhadarka <92981317+ChandniBhadarka@users.noreply.github.com>
2026-03-20 21:35:08 +0530

    Time: 1067 ms (36.79%), Space: 21.2 MB (66.6%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,62 @@
+##### Bubble Sort ######
+
+# class Solution(object):
+#     def sortArray(self, nums):
+#         n = len(nums)
+        
+#         for i in range(n):
+#             # Last i elements are already sorted
+#             for j in range(0, n - i - 1):
+                
+#                 if nums[j] > nums[j + 1]:
+#                     # Swap
+#                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
+        
+#         return nums
+
+# class Solution(object):
+#     def sortArray(self, nums):
+#         n = len(nums)
+        
+#         for i in range(n):
+#             swapped = False
+            
+#             for j in range(0, n - i - 1):
+#                 if nums[j] > nums[j + 1]:
+#                     nums[j], nums[j + 1] = nums[j + 1], nums[j]
+#                     swapped = True
+            
+#             if not swapped:
+#                 break
+        
+#         return nums
+
+####### Merge/ Quick Sort ####
+
+class Solution(object):
+    def sortArray(self, nums):
+        if len(nums) <= 1:
+            return nums
+        
+        mid = len(nums) // 2
+        left = self.sortArray(nums[:mid])
+        right = self.sortArray(nums[mid:])
+        
+        return self.merge(left, right)
+    
+    def merge(self, left, right):
+        result = []
+        i = j = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                result.append(left[i])
+                i += 1
+            else:
+                result.append(right[j])
+                j += 1
+        
+        result.extend(left[i:])
+        result.extend(right[j:])
+        
+        return result
